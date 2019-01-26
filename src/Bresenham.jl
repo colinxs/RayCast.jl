63feaea8e0a17192a4218d933656b8b0a5292fa6
Colin Summers <me@colinxsummers.com>
2019-01-25 20:28:14 -0800

    add tests, update tomls, fix allocation bug

--- src/Bresenham.jl
+++ src/Bresenham.jl
@@ -1,123 +1,69 @@
 module Bresenham
 
-export calc_ranges!
+export cast, cast_heading
 
-using Distances
-using UnsafeArrays
 using StaticArrays
 using RoboLib.Util: rangebearing2point
 
-#TODO(cxs): decide typing or nah
-
-@inline function cast_heading(I::Type{<:Integer}, x0::Real, y0::Real, heading::Real, maxrange::Real, occupied::Function)
+@inline function cast_heading(I::Type{<:Integer}, x0::Real, y0::Real, heading::Real, maxrange::Real)
     x1, y1 = rangebearing2point(x0, y0, heading, maxrange)
-    cast(I, x0, y0, x1, y1, occupied::Function)
+    cast(I, x0, y0, x1, y1)
 end
 
-@inline function cast_heading(x0::Real, y0::Real, heading::Real, maxrange::Real, occupied::Function)
-    cast_heading(Int, x0, y0, heading, maxrange, occupied)
+@inline function cast_heading(x0::Real, y0::Real, heading::Real, maxrange::Real)
+    cast_heading(Int, x0, y0, heading, maxrange)
 end
 
-@inline cast(x0, y0, x1, y1, maxx, maxy, maxrange, occupied::Function) = cast(Int, x0, y0, x1, y1, occupied::Function)
+@inline cast(x0, y0, x1, y1) = cast(Int, x0, y0, x1, y1)
 
-@inline function cast(I::Type{<:Integer}, x0, y0, x1, y1, occupied::Function)
-    cast(round(I, x0), round(I, y0), round(I, x1), round(I, y1), occupied)
+@inline function cast(I::Type{<:Integer}, x0, y0, x1, y1)
+    cast(round(I, x0), round(I, y0), round(I, x1), round(I, y1))
 end
 
-function cast(x0::I, y0::I, x1::I, y1::I, occupied::Function) where I <: Integer
+@inline function cast(x0::I, y0::I, x1::I, y1::I, occupied)::SVector{2, I} where I <: Integer
     dx = abs(x1 - x0)
     dy = abs(y1 - y0)
 
     xstep = ifelse(x0 < x1, one(I), -one(I))
     ystep = ifelse(y0 < y1, one(I), -one(I))
 
-    #if dy > dx
-    #   return _bline_high(x0, y0, x1, y1, dx, dy, xstep, ystep, occupied)
-    #else
-    #   return _bline_low(x0, y0, x1, y1, dx, dy, xstep, ystep, occupied)
-    #end
-    steep = dy > dx
-    #x0, y0 = ifelse(steep, (y0, x0), (x0, y0))
-    #x1, y1 = ifelse(steep, (y1, x1), (x1, y1))
-    #xstep, ystep = ifelse(steep, (ystep, xstep), (xstep, ystep))
-    #dx, dy = ifelse(steep, (dy, dx), (dx, dy))
-    x0, y0, x1, y1, xstep, ystep, dx, dy = ifelse(steep, (y0, x0, y1, x1, ystep, xstep, dy, dx), (x0, y0, x1, y1, xstep, ystep, dy, dx))
-    #testoccupied(x,y) = ifelse(steep, occupied(y,x), occupied(x,y))
-    #test = ifelse(steep, (x,y)->occupied(y,x), (x,y)->occupied(x,y))
-    #if steep
-    #    testoccupied(x,y) = occupied(y,x)
-    #else
-    #    testoccupied(x,y) = occupied(x,y)
-    #end
-    #@println(testoccupied)
-
-    x, y = _bline_low(x0, y0, x1, y1, dx, dy, xstep, ystep, occupied)
-    #x, y = ifelse(steep, (y, x), (x, y))
-    return x, y
+    if dy > dx
+       _bline_high(x0, y0, x1, y1, dx, dy, xstep, ystep, occupied)
+    else
+       _bline_low(x0, y0, x1, y1, dx, dy, xstep, ystep, occupied)
+    end
 end
 
-#function _bline_high(x0::I, y0::I, x1::I, y1::I, dx::I, dy::I, xstep::I, ystep::I, occupied::F) where I <: Integer where F <: Function
-#    err = convert(I, 2)*dx - dy
-#    x, y = x0, y0
-#    #y1 = max(one(I), min(y1, maxy))
-#    for yi=y0:ystep:y1
-#        y = yi
-#        #if x < one(I) return x + one(I), y
-#        #elseif x > maxx return x - one(I), y
-#        #elseif (x-x0)^2 + (y-y0)^2 > maxrange2 return x, y
-#        #elseif occupied(x, y) return x, y
-#        #end
-#        if (err > zero(I))
-#            x += xstep
-#            err -= convert(I, 2)*dy
-#        end
-#        err += convert(I, 2)*dx
-#    end
-#    return x, y
-#end
-#function _bline_high2(x0::I, y0::I, x1::I, y1::I, dx::I, dy::I, xstep::I, ystep::I, occupied::F) where I <: Integer where F <: Function
-#    err = convert(I, 2)*dx - dy # 2dy -dx
-#    x, y = x0, y0
-#    #y1 = max(one(I), min(y1, maxy))
-#    for yi=y0:ystep:y1 #xi=x0:xstep:x1
-#        y = yi # x = xi
-#        #if x < one(I) return x + one(I), y # x, y+ 1
-#        #elseif x > maxx return x - one(I), y
-#        #elseif (x-x0)^2 + (y-y0)^2 > maxrange2 return x, y
-#        #elseif occupied(x, y) return x, y
-#        #end
-#        if (err > zero(I))
-#            x += xstep # y+=ystep
-#            err -= convert(I, 2)*dy # err -dx
-#        end
-#        err += convert(I, 2)*dx
-#    end
-#    return x, y
-#end
+function _bline_high(x0::I, y0::I, x1::I, y1::I, dx::I, dy::I, xstep::I, ystep::I, occupied::Function)::SVector{2, I} where I <: Integer
+    err = I(2)*dx - dy
+    x, xn, y = x0, x0, y0
+    for yn=y0:ystep:y1
+        x = xn
+        y = yn
+        if occupied(x, y) return SVector(x, y) end
+        if (err > zero(I))
+            xn += xstep
+            err -= I(2)*dy
+        end
+        err += I(2)*dx
+    end
+    return SVector(x, y)
+end
 
-function _bline_low(x0::I, y0::I, x1::I, y1::I, dx::I, dy::I, xstep::I, ystep::I, occupied::F) where I <: Integer where F <: Function
-    err = convert(I, 2)*dy - dx
-    #x0+=xstep
+function _bline_low(x0::I, y0::I, x1::I, y1::I, dx::I, dy::I, xstep::I, ystep::I, occupied::Function)::SVector{2, I} where I <: Integer
+    err = I(2)*dy - dx
     x, y, yn = x0, y0, y0
-    #println("start")
-    #println(err)
-    #x1 = max(one(I), min(x1, maxx))
     for xn=x0:xstep:x1
         x = xn
         y = yn
-        #println("x, ",x," y, ", y, " err, ", err)
-        #if y < one(I) return x, y + one(I)
-        #elseif y > maxy return x, y - one(I)
-        #elseif (x-x0)^2 + (y-y0)^2 > maxrange2 return x, y
-        if occupied(x, y) return x, y end
-        #end
+        if occupied(x, y) return SVector(x, y) end
         if (err > zero(I))
             yn += ystep
-            err -= convert(I, 2)*dx
+            err -= I(2)*dx
         end
-        err += convert(I, 2)*dy
+        err += I(2)*dy
     end
-    return x, y
+    return SVector(x,y)
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,22 +1,73 @@
 using Test
-using RayCast.Bresenham: cast_heading
 
-@testset "RayCast" begin
+using RayCast.Bresenham: cast_heading, cast
+using StaticArrays
 
-#Todo(cxs): move Test package to extras
+@inline function occupied(x,y)::Bool
+    return false
+end
 
-@test begin
-    thetas = LinRange(0, 2*pi, 100)
-    sidemax = ceil(sqrt(100^2/2))
-    rmax = sqrt(2*sidemax^2)
-    for theta in thetas
-        x, y = cast_heading(0,0,theta,100, (x,y)->false)
-        r = sqrt(x^2 + y^2)
-        if r > rmax return false end
+function alloc_test()
+    n, r = 360, 100
+    thetas = collect(LinRange(0, 2*pi, n))
+    sc = sincos.(thetas)
+    xy = [SVector(trunc(Int, c*r), trunc(Int, s*r)) for (s, c) in sc]
+    buf = [SVector(0,0) for _ in 1:n]
+    alloc = @allocated _alloc_test(xy, buf)
+    return iszero(alloc)
+end
+
+function _alloc_test(xy, buf)
+    for i in 1:length(xy)
+        x, y = xy[i]
+        buf[i] = cast(1, 1, x, y, occupied)
+    end
+end
+
+function validate_line()
+    r = 10
+    for (i, theta) in enumerate(LinRange(0,2*pi, 36))
+        s, c = sincos(theta)
+
+        # target endpoints
+        x = round(Int, r * c)
+        y = round(Int, r * s)
+
+        # build line eq
+        dx = x
+        dy = y
+        function errfunc(x,y)
+            # Ax + By + C where C==0 b/c no y intercept
+            # pos iff below line
+            dy*x - dx*y
+        end
+
+        # test that the algorithm correctly follows the line
+        bounded = true
+        # maxerror follows directly from Bresenham line algorithm
+        maxerror = max(abs(dx), abs(dy))
+        function test(x,y)
+            err = errfunc(x,y)
+            bounded &= abs(err) <= maxerror
+            return false
+        end
+
+        # computed endpoints
+        xn, yn = cast(0,0,x,y, test)
+
+        if x != xn || y != yn
+            return false
+        elseif  !bounded
+            return false
+        end
     end
     return true
 end
 
+@testset "Bresenham" begin
+
+    @test validate_line()
+    @test alloc_test()
 end
 
 
