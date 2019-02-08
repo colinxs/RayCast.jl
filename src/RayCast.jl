5f926f96e54277a8f4fbdc3eca65205f890b12b8
Colin Summers <me@colinxsummers.com>
2019-02-07 23:01:02 -0800

    change git url to https

--- src/RayCast.jl
+++ src/RayCast.jl
@@ -1,5 +1,9 @@
 module RayCast
 
+using Logging, LoggingExtras
+const LOGGER = DemuxLogger(FileLogger(joinpath(@__DIR__, "logs/log.txt")))
+global_logger(LOGGER)
+
 include("Bresenham.jl")
 
 end # module
