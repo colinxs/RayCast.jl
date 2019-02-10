f34429864e1edc86ca44b53119669a03e822bb91
Colin Summers <me@colinxsummers.com>
2019-02-09 16:12:57 -0800

    temporarily disable logging

--- src/RayCast.jl
+++ src/RayCast.jl
@@ -1,8 +1,8 @@
 module RayCast
 
-using Logging, LoggingExtras
-const LOGGER = DemuxLogger(FileLogger(joinpath(@__DIR__, "logs/log.txt")))
-global_logger(LOGGER)
+#using Logging, LoggingExtras
+#const LOGGER = DemuxLogger(FileLogger(joinpath(@__DIR__, "logs/log.txt")))
+#global_logger(LOGGER)
 
 include("Bresenham.jl")
 
